2605b4c55b0d95e971e88d449c6c6766716ab789
bojourmoon <misssozolina@gmail.com>
2018-05-23 20:30:06 +0300

    diena2

---
+++ diena2/src/diena2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// ja if neejam ieksa tad izpildas else un ja neiens no elsiem neizpildas tad vienmer pedejais izp.
+//jebkura if var rakstit citu if, pirmai variants, tad daram to , ja ne tad so, 
+public class diena2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(" ievadi lûdzu skaitli");
+		int somenumber = scanner.nextInt();
+if (somenumber >= 10 ) {
+	System.out.println(" ir lielaks par  10");
+	somenumber = 10;
+}else{
+	System.out.println(" navaaa 10");
+}
+System.out.println(" absolûti navaaa 10");
+}
+	}
+
+
